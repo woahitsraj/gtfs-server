af6c560532c41c6db652a3bcedefecabab444278
Rajan Singh <rajan1992@hotmail.com>
2016-06-22 15:20:13 -0400

    cleanup javascript

--- routes/api.js
+++ routes/api.js
@@ -4,46 +4,46 @@ var gtfs = require('gtfs');
 var router = express.Router();
 
 router.route('/stops')
-	.get(function(req, res) {
-		var agency_key;
-		gtfs.agencies(function(err, agencies) {
-			if (err) {
-				console.log(err);
-			} else {
-				agency_key = agencies[0].agency_key;
-				gtfs.getStops(agency_key, function(err, stops) {
-					if (err) {
-						console.log(err);
-					} else {
-						res.send(stops);
-					}
-				});
+    .get(function(req, res) {
+        var agency_key;
+        gtfs.agencies(function(err, agencies) {
+            if (err) {
+                console.log(err);
+            } else {
+                agency_key = agencies[0].agency_key;
+                gtfs.getStops(agency_key, function(err, stops) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        res.send(stops);
+                    }
+                });
 
-			}
-		});
-	});
+            }
+        });
+    });
 
 router.route('/stops/:stop')
-	//returns a particular stop
-	.get(function(req, res) {
-		var stop = req.params.stop;
-		var agency_key;
-		gtfs.agencies(function(err, agencies) {
-			if (err) {
-				console.log(err);
-			} else {
-				agency_key = agencies[0].agency_key;
-				gtfs.getStops(agency_key, stop, function(err, stops) {
-					if (err) {
-						console.log(err);
-					} else {
-						res.send(stops);
-					}
-				});
+    //returns a particular stop
+    .get(function(req, res) {
+        var stop = req.params.stop;
+        var agency_key;
+        gtfs.agencies(function(err, agencies) {
+            if (err) {
+                console.log(err);
+            } else {
+                agency_key = agencies[0].agency_key;
+                gtfs.getStops(agency_key, stop, function(err, stops) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        res.send(stops);
+                    }
+                });
 
-			}
-		});
+            }
+        });
 
-	});
+    });
 
 module.exports = router;
